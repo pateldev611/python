50842b319e44bb39d6ffd6b743c6bf3e77f2b21d
pateldev611 <144149888+pateldev611@users.noreply.github.com>
2024-02-10 13:37:43 +0530

    Add files via upload

---
+++ area_calc.py
@@ -0,0 +1,8 @@
+# name=input('Tell me your name,punk:')
+# age=input('...and your age:')
+# print(name,'you are',age)
+
+#calc a area of a circle
+radius=input('Enter the radius of circle (m):')
+area=3.124*int(radius)**2
+print('The area of your circle is:',area)
