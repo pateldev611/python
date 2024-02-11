651cc0e371bda75815000d80f6cec897a3a1094b
pateldev611 <144149888+pateldev611@users.noreply.github.com>
2024-02-11 20:54:39 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,27 @@
+# age=int(input('enter your age:'))
+
+# if age<10:
+#     print('you are young ,strange one')
+    
+
+# elif age<40:
+#     print('the fire in u is strong,strange one')
+
+
+
+
+
+
+
+# else:
+#             print('you arre wise beyound doubt ,strange one')
+
+
+meaty = input('do you eat meat?(y/n):')
+
+if meaty=='y':
+    print('here is the meaty menu...')
+else:
+    print('here is veggie menu...')    
+
+    
---
+++ loops.py
@@ -0,0 +1,13 @@
+ninjas=['ryu','crystal','yoshi','ken']
+
+# for ninja in ninjas:
+#     print(ninja)
+
+# for ninja in ninjas[1:3]:
+#      print(ninja)
+
+for ninja in ninjas:
+    if ninja == 'yoshi':
+        print(f'{ninja}-black-belt')
+    else:
+        print(ninja)
